74e70b1a91002623bc5ffa8299140949d5cb84cc
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-07 23:15:34 +0100

    .

--- odin todo-list/index.js
+++ odin todo-list/index.js
@@ -1,18 +1,20 @@
 const todolistLibrary = []
 const taskCount = document.querySelector('.task-count')
 
-
+//automate task count
 taskCount.innerHTML = `${todolistLibrary.length} tasks`
+
+//create class prototype for todolist
 class TodolistClass {
     constructor(title, description) {
         this.title = title
         this.description = description
     }
-
-    newTodo() {
-        const newTodo = new TodolistClass(this.title, this.description)
-        todolistLibrary.push(newTodo)
-        console.log(todolistLibrary)
-    }
 }
 
+// function to create new todolist and add to library
+function addTodoToLibrary(title, description) {
+    const newTodo = new TodolistClass(this.title, this.description)
+    todolistLibrary.push(newTodo)
+    console.log(todolistLibrary)
+}
