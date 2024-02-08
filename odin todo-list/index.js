01c451489c4791e9e8e4ac9667873a8a1b0f3e0a
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-08 01:10:35 +0100

    .

--- odin todo-list/index.js
+++ odin todo-list/index.js
@@ -3,4 +3,5 @@ import {addTodoToLibrary, articles, taskCountFunc} from "./page-load.js";
 taskCountFunc()
 addTodoToLibrary('wash', 'to wash cloth')
 addTodoToLibrary('goat', 'bleed')
+addTodoToLibrary('Go home', 'At this point you’ve already had a fair amount of practice using the various techniques we’ve shown you')
 articles()
