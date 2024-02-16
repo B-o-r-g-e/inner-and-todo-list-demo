3a1721750777b1a52c338fd47afcd16acae7f714
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-16 22:09:48 +0100

    .

--- api-lesson/weather.js
+++ api-lesson/weather.js
@@ -2,16 +2,19 @@ const img = document.querySelector('.weather-icon')
 const state = document.querySelector('.state')
 const country = document.querySelector('.country')
 const weatherContainer = document.querySelector('.weather-condition')
+const feelsLike = document.querySelector('.feels-like')
 
 async function getWeather() {
     try {
-        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=8864366c27ee44c9bfd21149241502&q=Lagos&aqi=no', {mode: "cors"})
+        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=8864366c27ee44c9bfd21149241502&q=London&aqi=no', {mode: "cors"})
         const weatherData = await response.json()
 
         state.innerHTML = weatherData.location.region
         country.innerHTML = weatherData.location.country
         img.src = weatherData.current.condition.icon
-        weatherContainer.innerHTML = `${weatherData.current.temp_c}&deg`
+        weatherContainer.innerHTML = `${weatherData.current.temp_c}&deg;`
+        feelsLike.innerHTML = `${weatherData.current.feelslike_c}&deg;`
+
         console.log(weatherData)
     } catch (e) {
         alert(e)
