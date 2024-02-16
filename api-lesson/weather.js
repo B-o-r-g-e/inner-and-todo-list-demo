a8dae3f10a7c989085611f6fe41d8e9f79775981
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-16 21:36:02 +0100

    .

---
+++ api-lesson/weather.js
@@ -0,0 +1,21 @@
+const img = document.querySelector('.weather-icon')
+const state = document.querySelector('.state')
+const country = document.querySelector('.country')
+const weatherContainer = document.querySelector('.weather-condition')
+
+async function getWeather() {
+    try {
+        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=8864366c27ee44c9bfd21149241502&q=Lagos&aqi=no', {mode: "cors"})
+        const weatherData = await response.json()
+
+        state.innerHTML = weatherData.location.region
+        country.innerHTML = weatherData.location.country
+        img.src = weatherData.current.condition.icon
+        weatherContainer.innerHTML = `${weatherData.current.temp_c}&deg`
+        console.log(weatherData)
+    } catch (e) {
+        alert(e)
+    }
+}
+
+getWeather()
