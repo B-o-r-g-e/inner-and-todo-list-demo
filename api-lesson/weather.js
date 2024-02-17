cff75ebcd48d4e82c7a63dafa8866665ae767743
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-17 06:47:44 +0100

    .

--- api-lesson/weather.js
+++ api-lesson/weather.js
@@ -6,8 +6,9 @@ const feelsLike = document.querySelector('.feels-like')
 
 async function getWeather() {
     try {
-        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=8864366c27ee44c9bfd21149241502&q=Lagos&aqi=no', {mode: "cors"})
+        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=8864366c27ee44c9bfd21149241502&q=Washington&aqi=no', {mode: "cors"})
         const weatherData = await response.json()
+        const condition = weatherData.current.condition.text
 
         state.innerHTML = weatherData.location.region
         country.innerHTML = weatherData.location.country
@@ -15,7 +16,12 @@ async function getWeather() {
         weatherContainer.innerHTML = `${weatherData.current.temp_c}&deg;`
         feelsLike.innerHTML = `${weatherData.current.feelslike_c}&deg;`
 
+        if (condition === 'Overcast') {
+            console.log('done')
+        }
+
         console.log(weatherData)
+        console.log(condition)
     } catch (e) {
         alert(e)
     }
