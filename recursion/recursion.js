2f2de580a6048751e98249f18fb3bb7a89d9ad29
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-21 03:10:01 +0100

    .

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -4,8 +4,12 @@ function fibs(n) {
     } else if (n <= 1) {
         return [0, 1]
     } else {
+        let result  = [0, 1]
         for (let i = 2; i < n; i++) {
-            let result  = [0, 1]
+            result.push(result[i - 1] + result [i - 2])
         }
+        return result
     }
 }
+
+fibs()
