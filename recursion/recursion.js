8d17b9d0538d961401e3d46ee644d3fc364392ef
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-21 04:03:51 +0100

    .

--- recursion/recursion.js
+++ recursion/recursion.js
@@ -16,19 +16,11 @@ function fibs(n) {
     }
 }
 
-function fibsRec(n) {
-    if (n <= 0) {
-        return []
-    } else if (n === 1) {
-        return [0]
-    } else if (n === 2) {
-        return [0, 1]
-    } else {
-        let result = [0, 1]
-        fibsRec(n)
+function fibsRec(n, current  = 0, next = 1, result = []) {
+    if (n < 2) {
         return result
     }
 }
 
-fibs(8)
-fibsRec(8)
+fibs(3)
+// fibsRec(8)
