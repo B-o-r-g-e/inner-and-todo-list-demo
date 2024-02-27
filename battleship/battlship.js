f64e8ce038dd727e51af7b665b57d7e72fb5fa78
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-02-28 00:56:27 +0100

    .

--- battleship/battlship.js
+++ battleship/battlship.js
@@ -31,3 +31,62 @@ test('Ship isSunk function', () => {
     ship.hit();
     expect(ship.isSunk()).toBe(true);
 });
+
+class Gameboard {
+    constructor() {
+        this.ships = [];
+        this.missedAttacks = [];
+    }
+
+    placeShip(ship, coordinates) {
+        this.ships.push({ ship, coordinates });
+    }
+
+    receiveAttack(coordinates) {
+        const hitShip = this.ships.find(({ coordinates }) =>
+            coordinates.some(coord => coord[0] === coordinates[0] && coord[1] === coordinates[1])
+        );
+
+        if (hitShip) {
+            hitShip.ship.hit();
+        } else {
+            this.missedAttacks.push(coordinates);
+        }
+    }
+
+    allShipsSunk() {
+        return this.ships.every(({ ship }) => ship.isSunk());
+    }
+}
+
+// Unit tests
+test('Gameboard placeShip function', () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(3);
+    gameboard.placeShip(ship, [[0, 0], [0, 1], [0, 2]]);
+    expect(gameboard.ships.length).toBe(1);
+});
+
+test('Gameboard receiveAttack function', () => {
+    const gameboard = new Gameboard();
+    const ship = new Ship(3);
+    gameboard.placeShip(ship, [[0, 0], [0, 1], [0, 2]]);
+    gameboard.receiveAttack([0, 0]);
+    expect(ship.hits).toBe(1);
+    gameboard.receiveAttack([1, 0]);
+    expect(gameboard.missedAttacks.length).toBe(1);
+});
+
+test('Gameboard allShipsSunk function', () => {
+    const gameboard = new Gameboard();
+    const ship1 = new Ship(2);
+    const ship2 = new Ship(3);
+    gameboard.placeShip(ship1, [[0, 0], [0, 1]]);
+    gameboard.placeShip(ship2, [[1, 0], [1, 1], [1, 2]]);
+    ship1.hit();
+    ship1.hit();
+    ship2.hit();
+    ship2.hit();
+    ship2.hit();
+    expect(gameboard.allShipsSunk()).toBe(true);
+});
